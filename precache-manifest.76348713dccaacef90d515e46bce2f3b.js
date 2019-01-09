ca2a243a50aadf4d1728c9394f371d5848d57b9f
Sthefanny <s.gonzaga@cinq.com.br>
2019-01-09 09:03:51 -0200

    Updates

--- precache-manifest.76348713dccaacef90d515e46bce2f3b.js
+++ precache-manifest.76348713dccaacef90d515e46bce2f3b.js
@@ -4,24 +4,24 @@ self.__precacheManifest = [
     "url": "/curriculum/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "fcbd537114c2e93188ee",
-    "url": "/curriculum/static/css/main.13d26c05.chunk.css"
+    "revision": "949f94d8d1dab725fd7f",
+    "url": "/curriculum/static/css/main.93bf92cd.chunk.css"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
     "url": "/curriculum/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "6df3b0c0bcbb72e1c668",
-    "url": "/curriculum/static/js/1.6df3b0c0.chunk.js"
+    "revision": "bf49eff046791372068d",
+    "url": "/curriculum/static/js/1.bf49eff0.chunk.js"
   },
   {
     "revision": "2fae871c0e353310d84d",
     "url": "/curriculum/static/js/runtime~main.2fae871c.js"
   },
   {
-    "revision": "fcbd537114c2e93188ee",
-    "url": "/curriculum/static/js/main.fcbd5371.chunk.js"
+    "revision": "949f94d8d1dab725fd7f",
+    "url": "/curriculum/static/js/main.949f94d8.chunk.js"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "/curriculum/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
-    "revision": "6df3b0c0bcbb72e1c668",
-    "url": "/curriculum/static/css/1.5c453991.chunk.css"
+    "revision": "bf49eff046791372068d",
+    "url": "/curriculum/static/css/1.18291157.chunk.css"
   },
   {
-    "revision": "41ea3c195c106b7303c117aa9ce94319",
+    "revision": "74b70d09f6744c0a777b09ce881df72a",
     "url": "/curriculum/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/curriculum/precache-manifest.91b701de36e9dafd4f490bf7593c98c6.js"
+  "/curriculum/precache-manifest.76348713dccaacef90d515e46bce2f3b.js"
 );
 
 workbox.clientsClaim();
